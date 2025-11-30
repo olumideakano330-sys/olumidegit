57d62345c309008f26d165d43631264d6621841b
olumideakano330-sys <olumideakano330@gmail.com>
2025-11-30 22:48:38 +0100

    Create physics _formula.py

---
+++ physics _formula.py
@@ -0,0 +1,12 @@
+print("..........welcome to formula chooser.........")
+choice = input("choose a formula (1-4)")
+if choice == "1":
+    print("your using formula A")
+elif choice == "2":
+    print("your using formula B")
+elif choice == "3":
+    print("your using formula C")
+elif choice == "4":
+    print("your using formula D")
+else:
+    print("invalid choice pls try aagain")
