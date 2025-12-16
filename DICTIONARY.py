1226492c981627efa3a59656cfa7e70c8521f505
olumideakano330-sys <olumideakano330@gmail.com>
2025-12-16 19:36:17 +0100

    Create DICTIONARY.py

---
+++ DICTIONARY.py
@@ -0,0 +1,24 @@
+yoruba_dict = {
+    'water': 'omi',
+    'work': 'ise',
+    'love': 'ife',
+    'day': 'day',
+    'name': 'oruko',
+    'house': 'ile',
+    'money': 'owo',
+    'food': 'ounje',
+    'road': 'ona',
+    'cloth': 'aso',
+    'seed': 'irugbin',
+    'time': 'igba',
+    'book': 'iwe',
+    'representative': 'asoju',
+    'stength': 'agbara',
+    'sky': 'orun',
+    'world': 'aye',
+    'life': 'emi',
+    'walk': 'irin',
+    'end': 'ópin',
+}
+word = input("Enter a word: ")
+print(yoruba_dict[word])
